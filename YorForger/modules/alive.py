319fad3085204bf7de66b27e9a45c99a172d9e12
SenapiGod <108626834+SenapiGod@users.noreply.github.com>
2022-07-25 10:41:20 +0530

    Update alive.py

--- YorForger/modules/alive.py
+++ YorForger/modules/alive.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 PHOTO = "https://telegra.ph/file/976a903c3c286536ec646.jpg"
 
 def alive(update: Update, context: CallbackContext):
-    TEXT = f"ʜɪ, ɪ'ᴍ ɪᴛᴀᴄʜɪ ᴜᴄʜɪʜᴀ!\n\nɪ ᴡᴏʀᴋ ᴜɴᴅᴇʀ- **[999 Gang 亗](https://t.me/fed999wrld)** \n\nɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ"
+    TEXT = f"ʜɪ, ɪ'ᴍ ɪᴛᴀᴄʜɪ ᴜᴄʜɪʜᴀ!\n\nɪ ᴡᴏʀᴋ ᴜɴᴅᴇʀ- **[999 Gang 亗](https://t.me/Ft999_eXe)** \n\nɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ"
 
 
     update.effective_message.reply_photo(
@@ -19,10 +19,10 @@ def alive(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(
             [
                 [
-                InlineKeyboardButton(text="【Support】", url="https://t.me/fed999wrld"),
+                InlineKeyboardButton(text="【Support】", url="https://t.me/Ft999_eXe"),
                 InlineKeyboardButton(text="【Updates】", url="https://t.me/itachixobot_support")
                 ],
-                [InlineKeyboardButton(text="【999 Gang 亗】", url="https://t.me/fed999wrld")]
+                [InlineKeyboardButton(text="【999 Gang 亗】", url="https://t.me/Ft999_eXe")]
             ]
         ),
     )
